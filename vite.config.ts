913772f2cfaa3e1fce841a426f8b65db5a19a4a8
DanSnow <dododavid006@gmail.com>
2026-06-21 00:25:18 +0800

    chore(vite): configure tsconfig paths via resolve option

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import React from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [devtools(), tsconfigPaths(), tailwindcss(), tanstackStart({}), React(), nitro({ preset: 'node-server' })],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tailwindcss(), tanstackStart({}), React(), nitro({ preset: 'node-server' })],
 });
